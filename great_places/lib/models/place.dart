4db6aa4d5fa9288dc921cc6312600d9ced82aece
hrsydv1702 <hrsydv1702@gmail.com>
2021-05-12 22:18:11 +0530

    favouritePlace_app

---
+++ great_places/lib/models/place.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+  const PlaceLocation({
+    @required this.latitude,
+    @required this.longitude,
+    this.address,
+  });
+}
+
+class Place {
+  final id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    @required this.id,
+    @required this.title,
+    @required this.location,
+    @required this.image,
+  });
+}
